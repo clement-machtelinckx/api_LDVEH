c126c7d89768dfff919a5b521ea309d3436d9e48
clement-machtelinckx <clement.machtelinckx@laplateforme.io>
2025-06-17 10:57:58 +0200

    security

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -103,7 +103,7 @@ class SecurityController extends AbstractController
         $em->persist($user);
         $em->flush();
 
-        return $this->json(['message' => 'Utilisateur créé avec succès ✅']);
+        return $this->json(['message' => 'Utilisateur créé avec succès.']);
     }
 
     #[Route('/logout', name: 'app_logout', methods: ['GET'])]
