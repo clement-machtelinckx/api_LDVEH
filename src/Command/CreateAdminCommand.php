9fa096d46533cb68fccb291c190901581928b86f
clement-machtelinckx <clement.machtelinckx@laplateforme.io>
2025-07-04 15:23:26 +0200

    add comande admin

---
+++ src/Command/CreateAdminCommand.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+#[AsCommand(
+    name: 'app:create-admin',
+    description: 'Crée un utilisateur admin vite fait.',
+)]
+class CreateAdminCommand extends Command
+{
+    public function __construct(
+        private EntityManagerInterface $em,
+        private UserPasswordHasherInterface $passwordHasher
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('email', InputArgument::REQUIRED, 'Email de l\'admin')
+            ->addArgument('password', InputArgument::REQUIRED, 'Mot de passe');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $email = $input->getArgument('email');
+        $password = $input->getArgument('password');
+
+        $user = new User();
+        $user->setEmail($email);
+        $user->setRoles(['ROLE_ADMIN']);
+        $user->setPassword(
+            $this->passwordHasher->hashPassword($user, $password)
+        );
+
+        $this->em->persist($user);
+        $this->em->flush();
+
+        $output->writeln('<info>✅ Admin créé avec succès !</info>');
+
+        return Command::SUCCESS;
+    }
+}
