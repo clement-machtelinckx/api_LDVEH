6dd303f0eb488f1fe56c1b5ad85e15fda6940888
clement-machtelinckx <clement.machtelinckx@laplateforme.io>
2025-07-04 13:41:55 +0200

    deploiment

--- config/bundles.php
+++ config/bundles.php
@@ -17,5 +17,4 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['all' => true],
 ];
